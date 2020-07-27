f0e632e958c9c44b7c1b147f22a64ea0660b046e
zhuyuchen <zhuyuchen@histomed.com>
2020-07-27 09:31:35 +0800

    todo

--- src/main/java/com/example/demo/base/utils/MsgResult.java
+++ src/main/java/com/example/demo/base/utils/MsgResult.java
@@ -6,11 +6,14 @@ import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 
+// todo 代码规范，空行太多，idea下个插件 Alibaba Java Coding Guidelines
 
+// todo Data注解包含了 NoArgsConstructor
 @Data
 @Component
 @NoArgsConstructor
 public  class MsgResult {
+    // todo 封装太简单了，最好把Object改成泛型，用builder模式重新封装
     private Object data;
     private Integer code;
     private String message;
